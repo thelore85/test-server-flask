d00517a43ff5067e870225d47231807e2e0e4758
TheLore85 <garofalo.digital@gmail.com>
2024-06-25 22:16:09 +0200

    server: upload v7

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from api.routes import api
 
 
 def create_app():
+    print('... creating server app ... 2')
     app = Flask(__name__)
     app.register_blueprint(api, url_prefix='/api')
 
@@ -10,5 +11,5 @@ def create_app():
 
 
 if __name__ == "__main__":
-    print('... creating server app ...')
+    print('... creating server app ... 1')
     app = create_app()
