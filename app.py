396ae99868a3e94ecaea0c857113dcc5445ac41a
TheLore85 <garofalo.digital@gmail.com>
2024-06-25 22:30:52 +0200

    server: upload v13

--- app.py
+++ app.py
@@ -1,21 +1,3 @@
-# from flask import Flask
-# from api.routes import api
-
-
-# def create_app():
-#     print('running server app')
-#     app = Flask(__name__)
-#     app.register_blueprint(api, url_prefix='/api')
-
-#     return app
-
-# if __name__ == "__main__":
-#     app = create_app()
-
-
-
-
-
 from flask import Flask
 
 app = Flask(__name__)
