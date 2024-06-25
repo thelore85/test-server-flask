c9076ef170a65c3c065ff73f66748f4789105a97
TheLore85 <garofalo.digital@gmail.com>
2024-06-25 22:37:17 +0200

    server: upload v16

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
+# from flask import Flask
+# from api.routes import api
+
+
+# def create_app():
+#     print('running server app')
+#     app = Flask(__name__)
+#     app.register_blueprint(api, url_prefix='/api')
+
+#     return app
+
+# if __name__ == "__main__":
+#     app = create_app()
+
+
+
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.routes("/")
-def home():
+@app.route("/")
+def main():
     return 'server is running on main routh'
