59add83c7a46d6bf6094e40d567390a1b2a490a4
TheLore85 <garofalo.digital@gmail.com>
2024-06-25 22:12:50 +0200

    server: upload v6

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 from flask import Flask
 from api.routes import api
 
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'this is the back running'
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(api, url_prefix='/api')
+
+    return app
 
 
-# def create_app():
-#     app = Flask(__name__)
-#     app.register_blueprint(api, url_prefix='/api')
-
-#     return app
-
-# On server launch
-# if __name__ == "__main__":
-#     app = create_app()
+if __name__ == "__main__":
+    print('... creating server app ...')
+    app = create_app()
