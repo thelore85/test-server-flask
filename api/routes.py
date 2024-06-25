8ca1e2d8be6724d16a0a14acb840e0c7424fff28
TheLore85 <garofalo.digital@gmail.com>
2024-06-25 22:51:28 +0200

    server: upload v21

--- api/routes.py
+++ api/routes.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint
 
 # blueprint setting
 api = Blueprint('api', __name__)
 
 @api.route('/')
-def home():
+def main():
     return 'this is the back running'
+
+@api.route('/home')
+def home():
+    return 'HOME / this is the back running'
--- app.py
+++ app.py
@@ -2,8 +2,15 @@ from flask import Flask
 from api.routes import api
 
 
-app = Flask(__name__)
-app.register_blueprint(api, url_prefix='/api')
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(api, url_prefix='/api')
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+
 
 
 
