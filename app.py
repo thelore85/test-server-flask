8c8169846426ee10c27b98e5fa9b959d952f4885
TheLore85 <garofalo.digital@gmail.com>
2024-06-26 10:38:29 +0200

    server: upload v29

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from api.routes import api
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(api, url_prefix='/api')
+    # app.register_blueprint(api, url_prefix='/api')
 
     @app.route('/')
     def index():
-        return render_template('index.html')
+        return 'redering server main route'
 
     return app
 
