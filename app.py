f5d5b1e5e081e3deffba2c15eb3e8f8cbbdf9b63
TheLore85 <garofalo.digital@gmail.com>
2024-06-26 10:48:52 +0200

    server: upload v32

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from api.routes import api
 
 def create_app():
     app = Flask(__name__)
-    # app.register_blueprint(api, url_prefix='/api')
+    app.register_blueprint(api, url_prefix='/api')
 
+    # Interface
     @app.route('/')
     def index():
-        return 'redering server main route'
+        return render_template('index.html')
+    
 
     return app
 
