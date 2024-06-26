50bf52feb4fd6bf0bf6727900e6c0fed94d765d8
TheLore85 <garofalo.digital@gmail.com>
2024-06-26 13:09:31 +0200

    db sql initializtion v7

--- app.py
+++ app.py
@@ -7,26 +7,25 @@ from api.routes import api
 # from api.model import db
 
 # App - server
-def create_app():
-    app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://default:kVtLIcRm3o4s@ep-twilight-bird-a2o7tszp.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require"
-    
-    # Initialize SQLAlchemy and defining a simple Book model
-    # db = SQLAlchemy(app)
 
-    #setting
-    app.register_blueprint(api, url_prefix='/api')
-    setup_admin(app)
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://default:kVtLIcRm3o4s@ep-twilight-bird-a2o7tszp.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require"
+
+# Initialize SQLAlchemy and defining a simple Book model
+# db = SQLAlchemy(app)
+
+#setting
+app.register_blueprint(api, url_prefix='/api')
+setup_admin(app)
 
 
-    # Interface
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-   
-    # with app.app_context():
-    #     db.create_all()
+# Interface
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    return app
+# with app.app_context():
+#     db.create_all()
 
-app = create_app()
+if __name__ == "__main__":
+    app.run(debug=True)
