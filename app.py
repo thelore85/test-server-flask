8d88d86b536faf26fef824664ba57a33248aef4f
TheLore85 <garofalo.digital@gmail.com>
2024-06-26 10:05:09 +0200

    server: upload v24

--- app.py
+++ app.py
@@ -14,6 +14,27 @@ if __name__ == "__main__":
 
 
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return 'server is running on main routh'
+
+
+
+# server with flask not work as expected:
+
+# //////// project structure
+
+# // main folder
+# // - api
+# // -- route.py
+# // - app.py
+
+# # // this code works fine:
+
 # from flask import Flask
 
 # app = Flask(__name__)
@@ -21,3 +42,22 @@ if __name__ == "__main__":
 # @app.route("/")
 # def main():
 #     return 'server is running on main routh'
+
+
+
+# # ////////////this doesen't (but do locally)
+
+# from flask import Flask
+# from api.routes import api
+
+
+# def create_app():
+#     app = Flask(__name__)
+#     app.register_blueprint(api, url_prefix='/api')
+
+#     return app
+
+# if __name__ == "__main__":
+#     app = create_app()
+
+
